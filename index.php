9f948d6157f5a8e1b368f816dc96a8a2f0a67646
tanish <tanish@forkmedia.in>
2019-11-06 00:37:42 +0530

    Changes

--- index.php
+++ index.php
@@ -10,134 +10,94 @@
     <script src= "https://unpkg.com/masonry-layout@4.2.2/dist/masonry.pkgd.min.js"></script>
 
     <style>
-        * {
-            box-sizing: border-box;
+                 .masonry {
+            column-count: 4;
+            column-gap: 1em;
         }
 
+        .item {
+            background-color: #eee;
+            display: inline-block;
+            margin: 0 0 1em;
+            width: 100%;
+        }
         body {
+            font: 1em/1.67 'Open Sans', Arial, Sans-serif;
+            margin: 0;
+            background: #e9e9e9;
+        }
+
+        .wrapper {
+            width: 95%;
+            margin: 3em auto;
+        }
+
+        .masonry {
+            margin: 1.5em 0;
+            padding: 0;
+            -moz-column-gap: 1.5em;
+            -webkit-column-gap: 1.5em;
+            column-gap: 1.5em;
+            font-size: .85em;
+        }
+
+        .item {
+            display: inline-block;
             background: #fff;
-            font-family: 'Roboto', sans-serif;
-        }
-
-        .header {
-            position: relative;
-            padding: 20px;
-            margin: 50px auto;
-            text-align: center;
-
-            &:after {
-                content: "";
-                position: absolute;
-                display: block;
-                bottom: 0;
-                left: 50%;
-                transform: translateX(-50%);
-                height: 4px;
-                width: 25%;
-                background: #000;
-            }
-
-            h1 {
-                position: relative;
-                padding: 5px;
-            }
-        }
-
-        .container {
-            margin: 0 auto;
-        }
-
-        .item {
-            border-radius: 5px;
             padding: 1em;
-            margin: 0 auto 1em auto;
-            overflow: hidden;
-            z-index: 1;
-            text-decoration: none;
-            transition: all 120ms ease;
+            margin: 0 0 1.5em;
+            width: 100%;
+            -webkit-transition:1s ease all;
+            box-sizing: border-box;
+            -moz-box-sizing: border-box;
+            -webkit-box-sizing: border-box;
+            box-shadow: 2px 2px 4px 0 #ccc;
+        }
+        .item img{max-width:100%;}
 
-            h3 {
-                color: #000;
-            }
-
-            p {
-                color: #5b5b5b;
-            }
-
-            &:before {
-                content: "";
-                display: block;
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-                background: #f9f9f9;
-                z-index: -1;
-                transform: scale(0);
-                opacity: 0;
-                transition: all 100ms ease;
-            }
-
-            &:hover {
-                opacity: 0.75;
-            }
-
-            &:hover::before {
-                transform: scale(1);
-                opacity: 1;
-            }
-
-            img {
-                width: 100%;
-                height: auto;
-                display: block;
+        @media only screen and (min-width: 400px) {
+            .masonry {
+                -moz-column-count: 2;
+                -webkit-column-count: 2;
+                column-count: 2;
             }
         }
 
-        .grid-sizer,
-        .item {
-            width: 30%;
-        }
-
-        @media only screen and (min-width: 480px) and (max-width: 727px) {
-
-            .grid-sizer,
-            .item {
-                width: 33%;
+        @media only screen and (min-width: 700px) {
+            .masonry {
+                -moz-column-count: 3;
+                -webkit-column-count: 3;
+                column-count: 3;
             }
         }
 
-        @media only screen and (max-width: 479px) {
-
-            grid-sizer,
-            .item {
-                width: 50%;
+        @media only screen and (min-width: 900px) {
+            .masonry {
+                -moz-column-count: 4;
+                -webkit-column-count: 4;
+                column-count: 4;
             }
         }
 
-        .card {
-            box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-            transition: 0.3s;
+        @media only screen and (min-width: 1100px) {
+            .masonry {
+                -moz-column-count: 5;
+                -webkit-column-count: 5;
+                column-count: 5;
             }
+        }
 
-
-        .card:hover {
-            box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+        @media only screen and (min-width: 1280px) {
+            .wrapper {
+                width: 1260px;
             }
-
-        .mainContainer {
-            padding: 2px 16px;
-            background : white;
-            }
+        }
     </style>
 </head>
 
 <body>
 
-        <div class="container">
-            <div class="grid-sizer"></div>
-            <div class="gutter-sizer"></div>
+        <div class="masonry">
                 <?php
                         $data = file_get_contents("https://cdn.pinkvilla.com/feed/fashion-section.json");
                         $data = json_decode($data, true);
@@ -148,29 +108,16 @@
                         }
                         array_multisort($viewCount, SORT_ASC, $data);
                         foreach ($data as $row) {
-                            echo '<div class="card"><a class="item" href="https://pinkvilla.com'. $row["path"] . '">
-                            <img src="'. $row["imageUrl"] . '">
-                            <div class="mainContainer">
-                            <h4><b>'. $row["title"] . '</b></h4>
-                            <p>'. $row["viewCount"] . '</p>
-                            </div></a>
-                            </div>
-';
+                            echo '<a class="item" href="https://pinkvilla.com'. $row["path"] . '">
+                                    "<img src="'. $row["imageUrl"] .'">
+                                    "<p style="margin:10px 0; color:black">
+                                        <b>'. $row["title"] . '</b>
+                                    </p>
+                                </a>';
                 }
                 ?>
         
         </div>
-        <script>
-        jQuery(window).on('load', function () {
-            $('.container').masonry({
-                columnWidth: '.grid-sizer',
-                gutter: 30,
-                itemSelector: '.item',
-                percentPosition: 'true',
-                fitWidth: true,
-            });
-        });
-    </script>
 </body>
 
 </html>
